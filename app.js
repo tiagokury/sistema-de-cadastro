65cdf636ae6fe324e54f19fbc697d70cb018a2de
Tiago <80292247+tiagokury@users.noreply.github.com>
2025-02-21 19:00:21 -0300

    MAIS UM CONSOLE.LOG ADICIONADO NO APP.JS

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+// MAIS UM CONSOLE LOG DE TESTE :
+console.log("O tiago fez mais esse commit")
