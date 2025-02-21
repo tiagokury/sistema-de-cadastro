005aa3aad2d9c5082c01a3d9c3301c116f9c691f
tiagokury <tiagoktl@gmail.com>
2025-02-21 19:05:39 -0300

    ADICIONANDO MAIS UM CONSOLE.LOG

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 // MAIS UM CONSOLE LOG DE TESTE :
 console.log("O tiago fez mais esse commit")
+console.log("mais um console.log")
+
